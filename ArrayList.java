a2161100086886e3f16a4283fe52999b479ccba2
ujjwal490 <ujjwaltyagi0006@gmail.com>
2024-09-14 11:16:44 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class ArrayList {
+public static void main(string [] args){
+Arraylist<Integer>list=new Arraylist<integer> ();
+
+// add elements 
+list.add(0);
+list.add(2);
+list.add(3);
+  System.out.println(list);
+}
+}
+
